1c09396e142063dbd7586abe07f9148d2bc918bd
Grant Ammons <grant@grant.dev>
2026-04-23 06:37:15 -0400

    remove bots

--- bots/pulse/tools/example_tool.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CurrentTimeTool < Grantclaw::Tool
-  desc "Get the current date and time"
-
-  def call
-    Time.now.strftime("%A %B %d, %Y %I:%M %p %Z")
-  end
-end
